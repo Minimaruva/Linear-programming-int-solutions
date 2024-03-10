8fb9655d81e7e93d250cb07bdbaf4a47fa06d1ef
Min <107836940+Minimaruva@users.noreply.github.com>
2024-03-10 10:35:01 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,170 @@
+function max(inequalities, profitFunction){
+    let profit = 0;
+    let X = 0;
+    let Y = 0;
+    
+    if (inequalities.length==1){
+        for(let x=0; x<=200; x++){
+            for(let y=0; y<=200; y++){
+                    if(eval((inequalities[0].replace('x','*'+x)).replace('y','*'+y))){
+                        let temp = eval((profitFunction.replace('x','*'+x)).replace('y','*'+y));
+                            if(temp > profit){
+                                profit = temp;
+                                X = x;
+                                Y = y;
+                            }
+                    }
+                }
+                }
+            }
+    
+    else if(inequalities.length==2){
+        for(let x=0; x<=200; x++){
+            for(let y=0; y<=200; y++){
+                    if(eval((inequalities[0].replace('x','*'+x)).replace('y','*'+y))
+                    && eval((inequalities[1].replace('x','*'+x)).replace('y','*'+y))){
+                        let temp = eval((profitFunction.replace('x','*'+x)).replace('y','*'+y));
+                            if(temp > profit){
+                                profit = temp;
+                                X = x;
+                                Y = y;
+                            }
+                    }
+                }
+            }
+    }
+
+    else if(inequalities.length==3){
+        for(let x=0; x<=200; x++){
+            for(let y=0; y<=200; y++){
+                    if(eval((inequalities[0].replace('x','*'+x)).replace('y','*'+y))
+                    && eval((inequalities[1].replace('x','*'+x)).replace('y','*'+y))
+                    && eval((inequalities[2].replace('x','*'+x)).replace('y','*'+y))){
+                        let temp = eval((profitFunction.replace('x','*'+x)).replace('y','*'+y));
+                            if(temp > profit){
+                                profit = temp;
+                                X = x;
+                                Y = y;
+                            }
+                    }
+                }
+            }
+    }
+
+    else if(inequalities.length==4){
+        for(let x=0; x<=200; x++){
+            for(let y=0; y<=200; y++){
+                    if(eval((inequalities[0].replace('x','*'+x)).replace('y','*'+y))
+                    && eval((inequalities[1].replace('x','*'+x)).replace('y','*'+y))
+                    && eval((inequalities[2].replace('x','*'+x)).replace('y','*'+y))
+                    && eval((inequalities[3].replace('x','*'+x)).replace('y','*'+y))){
+                        let temp = eval((profitFunction.replace('x','*'+x)).replace('y','*'+y));
+                            if(temp > profit){
+                                profit = temp;
+                                X = x;
+                                Y = y;
+                            }
+                    }
+                }
+            }
+    }
+    
+    document.getElementById('result').textContent = "Max profit: "+profit + "\n x: "+X + "\n y: " + Y;
+}
+function min(inequalities, profitFunction){
+    let profit = 1000000;
+    let X = 0;
+    let Y = 0;
+    
+    if (inequalities.length==1){
+        for(let x=0; x<=200; x++){
+            for(let y=0; y<=200; y++){
+                    if(eval((inequalities[0].replace('x','*'+x)).replace('y','*'+y))){
+                        let temp = eval((profitFunction.replace('x','*'+x)).replace('y','*'+y));
+                            if(temp < profit){
+                                profit = temp;
+                                X = x;
+                                Y = y;
+                            }
+                    }
+                }
+                }
+            }
+    
+    else if(inequalities.length==2){
+        for(let x=0; x<=200; x++){
+            for(let y=0; y<=200; y++){
+                    if(eval((inequalities[0].replace('x','*'+x)).replace('y','*'+y))
+                    && eval((inequalities[1].replace('x','*'+x)).replace('y','*'+y))){
+                        let temp = eval((profitFunction.replace('x','*'+x)).replace('y','*'+y));
+                            if(temp < profit){
+                                profit = temp;
+                                X = x;
+                                Y = y;
+                            }
+                    }
+                }
+            }
+    }
+
+    else if(inequalities.length==3){
+        for(let x=0; x<=200; x++){
+            for(let y=0; y<=200; y++){
+                    if(eval((inequalities[0].replace('x','*'+x)).replace('y','*'+y))
+                    && eval((inequalities[1].replace('x','*'+x)).replace('y','*'+y))
+                    && eval((inequalities[2].replace('x','*'+x)).replace('y','*'+y))){
+                        let temp = eval((profitFunction.replace('x','*'+x)).replace('y','*'+y));
+                            if(temp < profit){
+                                profit = temp;
+                                X = x;
+                                Y = y;
+                            }
+                    }
+                }
+            }
+    }
+
+    else if(inequalities.length==4){
+        for(let x=0; x<=200; x++){
+            for(let y=0; y<=200; y++){
+                    if(eval((inequalities[0].replace('x','*'+x)).replace('y','*'+y))
+                    && eval((inequalities[1].replace('x','*'+x)).replace('y','*'+y))
+                    && eval((inequalities[2].replace('x','*'+x)).replace('y','*'+y))
+                    && eval((inequalities[3].replace('x','*'+x)).replace('y','*'+y))){
+                        let temp = eval((profitFunction.replace('x','*'+x)).replace('y','*'+y));
+                            if(temp < profit){
+                                profit = temp;
+                                X = x;
+                                Y = y;
+                            }
+                    }
+                }
+            }
+    }
+    
+    document.getElementById('result').textContent = "Max profit: "+profit + "\n x: "+X + "\n y: " + Y;
+}
+
+
+
+function solveLinearProgramming() {
+    
+    
+    let profitFunction = (document.getElementById('profitFunction').value).replace(';','');
+    let inequalities = (document.getElementById('inequalities').value).replace(';','');
+    // let optimizationType = document.querySelector('input[name="optimizationType"]:checked').value;
+    inequalities=inequalities.split(",");
+    // console.log(eval(inequalities[0].replace('x','*2')));
+    let ismax = document.getElementById("maximize").checked;
+    let ismin = document.getElementById("minimize").checked;
+    if(ismax){
+        max(inequalities, profitFunction); 
+    }
+    else if(ismin){
+        min(inequalities,profitFunction);
+    }
+    else{
+        alert('Please select optimisation type!')
+    }
+
+}
